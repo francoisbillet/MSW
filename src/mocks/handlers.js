262e30f1c90b3e55c0d44762509c29de573599ba
François Billet <francoisbillet@live.fr>
2023-12-06 18:16:48 +0100

    WIP migrate to v2 (post Charles)

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -3,6 +3,6 @@ import { http, HttpResponse } from "msw";
 export const handlers = [
   http.get("https://dummyjson.com/products/:productId", ({ params }) => {
     const { productId } = params;
-    return new HttpResponse.json({ id: productId, title: "My mocked product" });
+    return HttpResponse.json({ id: productId, title: "My mocked product" });
   }),
 ];
